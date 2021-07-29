f6e25f1a7228890488edb10132a7369129471934
Ankan Sharma <ankanroni3@gmail.com>
2021-07-29 21:14:25 +0530

    Added themes.dart

---
+++ mlflutter/lib/themes.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyTheme {
+  MyTheme._();
+  static Color kPrimaryColor = Color(0xffD32F2F);
+  static Color kPrimaryColorVariant = Color(0xff686795);
+  static Color kAccentColor = Color(0xffffa000);
+  static Color kAccentColorVariant = Color(0xffF7A3A2);
+  static Color kUnreadChatBG = Color(0xffEE1D1D);
+
+  static final TextStyle kAppTitle = GoogleFonts.satisfy(fontSize: 36);
+
+  static final TextStyle heading2 = TextStyle(
+    color: Color(0xff686795),
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    letterSpacing: 1.5,
+  );
+
+  static final TextStyle name = TextStyle(
+    color: Colors.white,
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 1.5,
+    fontFamily: GoogleFonts.playball().fontFamily
+  );
+
+  static final TextStyle chatSenderName = TextStyle(
+    color: Colors.white,
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 1.5,
+  );
+
+  static final TextStyle bodyText1 = TextStyle(
+      color: Color(0xffAEABC9),
+      fontSize: 15,
+      letterSpacing: 1.2,
+      fontWeight: FontWeight.w500);
+
+  static final TextStyle bodyTextMessage =
+      TextStyle(fontSize: 13, letterSpacing: 1.5, fontWeight: FontWeight.w600);
+
+  static final TextStyle bodyTextTime = TextStyle(
+    color: Color(0xffAEABC9),
+    fontSize: 11,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 1,
+  );
+}
