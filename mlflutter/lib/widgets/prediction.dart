24e9198ba01f3c700f971b08a42c7fa5435ddf28
Ankan Sharma <ankanroni3@gmail.com>
2021-07-30 23:42:29 +0530

    class label added

--- mlflutter/lib/widgets/prediction.dart
+++ mlflutter/lib/widgets/prediction.dart
@@ -18,13 +18,66 @@ class _PredictionState extends State<Prediction> {
             'Prediction',
             style: TextStyle(
               color: Colors.green[400],
-              fontSize: 30,
-              fontWeight: FontWeight.w800,
-              letterSpacing: 5,
-              fontFamily: GoogleFonts.allura().fontFamily,
+              fontSize: 24,
+              fontWeight: FontWeight.w300,
+              letterSpacing: 2,
+              fontStyle: FontStyle.italic,
+              fontFamily: GoogleFonts.kanit().fontFamily,
             ),
           ) ,
-        )
+        ),
+        Center(
+          child: Text(
+            'Covid 19: ',
+            style: TextStyle(
+              color: Colors.grey[700],
+              fontSize: 18,
+              fontWeight: FontWeight.w300,
+              letterSpacing: 1,
+              fontStyle: FontStyle.italic,
+              fontFamily: GoogleFonts.comicNeue().fontFamily,
+            ),
+          ) ,
+        ),
+        Center(
+          child: Text(
+            'Lung Opacity: ',
+            style: TextStyle(
+              color: Colors.grey[700],
+              fontSize: 18,
+              fontWeight: FontWeight.w300,
+              letterSpacing: 1,
+              fontStyle: FontStyle.italic,
+              fontFamily: GoogleFonts.comicNeue().fontFamily,
+            ),
+          ) ,
+        ),
+        Center(
+          child: Text(
+            'Viral Pneumonia: ',
+            style: TextStyle(
+              color: Colors.grey[700],
+              fontSize: 18,
+              fontWeight: FontWeight.w300,
+              letterSpacing: 1,
+              fontStyle: FontStyle.italic,
+              fontFamily: GoogleFonts.comicNeue().fontFamily,
+            ),
+          ) ,
+        ),
+        Center(
+          child: Text(
+            'Normal: ',
+            style: TextStyle(
+              color: Colors.grey[700],
+              fontSize: 18,
+              fontWeight: FontWeight.w300,
+              letterSpacing: 1,
+              fontStyle: FontStyle.italic,
+              fontFamily: GoogleFonts.comicNeue().fontFamily,
+            ),
+          ) ,
+        ),
       ]
     );
   }
