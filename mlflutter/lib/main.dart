b7eedb909057917e4df2e368491e21de6120d066
Ankan Sharma <ankanroni3@gmail.com>
2021-07-30 22:24:37 +0530

    Home Screen added

--- mlflutter/lib/main.dart
+++ mlflutter/lib/main.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:mlflutter/screen/home_screen.dart';
 import 'package:mlflutter/themes.dart';
 
 void main() {
   runApp(
     MaterialApp(
       theme: ThemeData(
-      primaryColor: MyTheme.kPrimaryColor,
-      accentColor: MyTheme.kAccentColor,
-      textTheme: GoogleFonts.merriweatherTextTheme(
-        //Theme.of(context).textTheme,
+        primaryColor: MyTheme.kPrimaryColor,
+        accentColor: MyTheme.kAccentColor,
+        textTheme: GoogleFonts.merriweatherTextTheme(
+          //Theme.of(context).textTheme,
+        ),
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      primarySwatch: Colors.blue,
-      visualDensity: VisualDensity.adaptivePlatformDensity,
-    ),
+      home: HomeScreen(),
     ),
   );
 }
