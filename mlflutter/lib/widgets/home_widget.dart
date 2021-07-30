230c9d23f864a8ff32b192ef1d3f55d1412fd4c5
Ankan Sharma <ankanroni3@gmail.com>
2021-07-30 23:08:35 +0530

    Home widget added

---
+++ mlflutter/lib/widgets/home_widget.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomeWidget extends StatefulWidget {
+  //const HomeWidget({ Key? key }) : super(key: key);
+
+  @override
+  _HomeWidgetState createState() => _HomeWidgetState();
+}
+
+class _HomeWidgetState extends State<HomeWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(height:50),
+        Center(
+          child: SizedBox(
+            width: 300,
+            height: 300,
+            child: Card(
+              child: Center(
+                child: Text(
+                  "Upload Image",
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                    fontSize: 30,
+                    fontFamily: GoogleFonts.satisfy(fontSize: 20).fontFamily,
+                    
+                  ),
+                ),
+              ),
+              elevation: 4,
+              shadowColor: Colors.black,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+                
+              ),
+            ),
+          ),
+        )
+
+      ],
+    );
+  }
+}
