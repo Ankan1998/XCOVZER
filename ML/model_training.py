13925ef144e4ee05c9fc0bf08bbff657949131ed
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-07-31 23:06:17 +0530

    model_training script completed

--- ML/model_training.py
+++ ML/model_training.py
@@ -1,12 +1,21 @@
 from model import model_fn
-model = model_fn()
 from callbacks import callback_func
+from dataset_prep import data_generator
 
-def training(train_gen, val_gen, epochs=20,callback_list=callbacks_list):
+DATA_PATH=''
+train_generator, val_generator = data_generator(data_path)
+model = model_fn()
+
+MODEL_PATH = 'saved_model/best_model'
+CSV_PATH = 'log.csv'
+PATIENCE = 4
+callbacks_list = callback_func(MODEL_PATH, CSV_PATH, PATIENCE)
+
+def training(train_generator, val_generator, epochs=20,callback_list=callbacks_list):
 
     model.fit_generator(
         train_generator,
-        validation_data=validation_generator,
+        validation_data=val_generator,
         epochs=30,
         verbose=2,
         callbacks=callbacks_list
