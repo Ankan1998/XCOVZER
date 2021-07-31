0894efd32bd5377de1032070d0ad811b2be0833e
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-07-31 23:05:58 +0530

    Changes in data_prep

--- ML/dataset_prep.py
+++ ML/dataset_prep.py
@@ -1,7 +1,7 @@
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 def data_generator(
-    data_path='path',
+    data_path,
     img_height=128,
     img_width=128,
     val_split=0.2,
@@ -9,14 +9,14 @@ def data_generator(
     ):
     train_datagen = ImageDataGenerator(validation_split=val_split)
     train_generator = train_datagen.flow_from_directory(
-        train_data_dir,
+        data_path,
         target_size=(img_height, img_width),
         batch_size=batch_size,
         class_mode='categorical',
         subset='training') # set as training data
     # Splitting images for validation set
     validation_generator = train_datagen.flow_from_directory(
-        train_data_dir, # same directory as training data
+        data_path, # same directory as training data
         target_size=(img_height, img_width),
         batch_size=batch_size,
         class_mode='categorical',
