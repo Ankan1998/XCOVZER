0c65a00ad7ec74d4e3b3bf23a500d89d752967a0
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-08-01 01:02:32 +0530

    Camera added and clicked image showing in the box

--- mlflutter/lib/widgets/home_widget.dart
+++ mlflutter/lib/widgets/home_widget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:mlflutter/widgets/prediction.dart';
+import 'dart:io';
 
 class HomeWidget extends StatefulWidget {
   //const HomeWidget({ Key? key }) : super(key: key);
@@ -10,80 +12,87 @@ class HomeWidget extends StatefulWidget {
 }
 
 class _HomeWidgetState extends State<HomeWidget> {
+  final picker = ImagePicker();
+  String pred;
+  String prob;
+  File image;
+  bool img_present = false;
+  
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        SizedBox(height:MediaQuery.of(context).size.height*0.05),
+        SizedBox(height: MediaQuery.of(context).size.height * 0.05),
         Center(
           child: SizedBox(
-            width: MediaQuery.of(context).size.width*0.75,
-            height: MediaQuery.of(context).size.height*0.35,
+            width: MediaQuery.of(context).size.width * 0.75,
+            height: MediaQuery.of(context).size.height * 0.35,
             child: Card(
               child: Center(
-                child: Text(
-                  "Upload Image",
-                  style: TextStyle(
-                    color: Colors.grey[500],
-                    fontSize: 30,
-                    fontFamily: GoogleFonts.satisfy(fontSize: 20).fontFamily,
-                    
-                  ),
-                ),
+                child: img_present
+                    ? Image.file(
+                      image,
+                      height: MediaQuery.of(context).size.height * 0.35,
+                      width: MediaQuery.of(context).size.width * 1,
+                      fit: BoxFit.fitWidth,
+                    )
+                    : Text(
+                        "Upload Image",
+                        style: TextStyle(
+                          color: Colors.grey[500],
+                          fontSize: 30,
+                          fontFamily:
+                              GoogleFonts.satisfy(fontSize: 20).fontFamily,
+                        ),
+                      ),
               ),
               elevation: 4,
               shadowColor: Colors.black,
               shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),              
+                borderRadius: BorderRadius.circular(10.0),
               ),
             ),
           ),
         ),
-        SizedBox(
-          height:MediaQuery.of(context).size.height*0.02
-        ),
-
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            OutlinedButton(
-              onPressed: () {},
-              child: Icon(
-                Icons.camera_alt,
-                color: Colors.teal[600]
-              ),
-              style: OutlinedButton.styleFrom(
-                shape: CircleBorder(),
-                padding: EdgeInsets.all(14),
-              ),
+        SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          OutlinedButton(
+            onPressed: () async {
+              final pickedimage = await picker.pickImage(
+                  source: ImageSource.camera,
+                  //maxWidth: MediaQuery.of(context).size.width * 1,
+                  //maxHeight: MediaQuery.of(context).size.width * 0.35,
+                  imageQuality: 100);
+              setState(() {
+                if (pickedimage != null) {
+                  img_present = true;
+                  image = File(pickedimage.path);
+                }
+              });
+            },
+            child: Icon(Icons.camera_alt, color: Colors.teal[600]),
+            style: OutlinedButton.styleFrom(
+              shape: CircleBorder(),
+              padding: EdgeInsets.all(14),
             ),
-            OutlinedButton(
-              onPressed: () {},
-              child: Icon(
-                Icons.image,
-                color: Colors.teal[600],
-              ),
-              style: OutlinedButton.styleFrom(
-                //backgroundColor: Colors.grey[200],
-                shape: CircleBorder(),
-                padding: EdgeInsets.all(14),
-              ),
+          ),
+          OutlinedButton(
+            onPressed: () {},
+            child: Icon(
+              Icons.image,
+              color: Colors.teal[600],
             ),
-
-
-
-
-          ]
-        ),
-        SizedBox(
-          height:MediaQuery.of(context).size.height*0.03
-        ),
+            style: OutlinedButton.styleFrom(
+              //backgroundColor: Colors.grey[200],
+              shape: CircleBorder(),
+              padding: EdgeInsets.all(14),
+            ),
+          ),
+        ]),
+        SizedBox(height: MediaQuery.of(context).size.height * 0.03),
         Prediction(),
-        SizedBox(
-          height: MediaQuery.of(context).size.height*0.05
-        ),
-
-
+        SizedBox(height: MediaQuery.of(context).size.height * 0.05),
       ],
     );
   }
