853a61ab0502daff185c14df70093dd69ee35f4e
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-08-01 14:20:07 +0530

    home screen UI change

--- mlflutter/lib/widgets/home_widget.dart
+++ mlflutter/lib/widgets/home_widget.dart
@@ -24,12 +24,13 @@ class _HomeWidgetState extends State<HomeWidget> {
     return Column(
       children: [
         SizedBox(height: MediaQuery.of(context).size.height * 0.05),
-        Center(
-          child: SizedBox(
-            width: MediaQuery.of(context).size.width * 0.75,
-            height: MediaQuery.of(context).size.height * 0.35,
-            child: Card(
-              child: Center(
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.7,
+              height: MediaQuery.of(context).size.height * 0.35,
+              child: Card(
                 child: img_present
                     ? Image.file(
                       image,
@@ -46,62 +47,110 @@ class _HomeWidgetState extends State<HomeWidget> {
                               GoogleFonts.satisfy(fontSize: 20).fontFamily,
                         ),
                       ),
-              ),
-              elevation: 4,
-              shadowColor: Colors.black,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
+                elevation: 4,
+                shadowColor: Colors.black,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
               ),
             ),
-          ),
+            Column(
+              //mainAxisAlignment: MainAxisAlignment.spaceBetween, 
+              children: [
+                OutlinedButton(
+                  onPressed: () async {
+                    final pickedimage = await picker.pickImage(
+                        source: ImageSource.camera,
+                        imageQuality: 100);
+                    setState(() {
+                      if (pickedimage != null) {
+                        img_present = true;
+                        image = File(pickedimage.path);
+                      }
+                    });
+                  },
+                  child: Icon(Icons.camera_alt, color: Colors.teal[600]),
+                  style: OutlinedButton.styleFrom(
+                    shape: CircleBorder(),
+                    padding: EdgeInsets.all(14),
+                  ),
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.1,),
+                OutlinedButton(
+                  onPressed: () async {
+                    final pickedimage = await picker.pickImage(
+                        source: ImageSource.gallery,
+                        imageQuality: 100);
+                    setState(() {
+                      if (pickedimage != null) {
+                        img_present = true;
+                        image = File(pickedimage.path);
+                      }
+                    });
+                  },
+                  child: Icon(
+                    Icons.image,
+                    color: Colors.teal[600],
+                  ),
+                  style: OutlinedButton.styleFrom(
+                    //backgroundColor: Colors.grey[200],
+                    shape: CircleBorder(),
+                    padding: EdgeInsets.all(14),
+                  ),
+                ),
+              ]
+            ),
+          ],
         ),
-        SizedBox(height: MediaQuery.of(context).size.height * 0.02),
-        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-          OutlinedButton(
-            onPressed: () async {
-              final pickedimage = await picker.pickImage(
-                  source: ImageSource.camera,
-                  imageQuality: 100);
-              setState(() {
-                if (pickedimage != null) {
-                  img_present = true;
-                  image = File(pickedimage.path);
-                }
-              });
-            },
-            child: Icon(Icons.camera_alt, color: Colors.teal[600]),
-            style: OutlinedButton.styleFrom(
-              shape: CircleBorder(),
-              padding: EdgeInsets.all(14),
-            ),
-          ),
-          OutlinedButton(
-            onPressed: () async {
-              final pickedimage = await picker.pickImage(
-                  source: ImageSource.gallery,
-                  imageQuality: 100);
-              setState(() {
-                if (pickedimage != null) {
-                  img_present = true;
-                  image = File(pickedimage.path);
-                }
-              });
-            },
-            child: Icon(
-              Icons.image,
-              color: Colors.teal[600],
-            ),
-            style: OutlinedButton.styleFrom(
-              //backgroundColor: Colors.grey[200],
-              shape: CircleBorder(),
-              padding: EdgeInsets.all(14),
-            ),
-          ),
-        ]),
-        SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+        
+
+        SizedBox(height: MediaQuery.of(context).size.height * 0.05),
         if (img_present) ...[Prediction(imageClicked: image,)],
         SizedBox(height: MediaQuery.of(context).size.height * 0.05),
       ],
     );
   }
 }
+
+        // Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+        //   OutlinedButton(
+        //     onPressed: () async {
+        //       final pickedimage = await picker.pickImage(
+        //           source: ImageSource.camera,
+        //           imageQuality: 100);
+        //       setState(() {
+        //         if (pickedimage != null) {
+        //           img_present = true;
+        //           image = File(pickedimage.path);
+        //         }
+        //       });
+        //     },
+        //     child: Icon(Icons.camera_alt, color: Colors.teal[600]),
+        //     style: OutlinedButton.styleFrom(
+        //       shape: CircleBorder(),
+        //       padding: EdgeInsets.all(14),
+        //     ),
+        //   ),
+        //   OutlinedButton(
+        //     onPressed: () async {
+        //       final pickedimage = await picker.pickImage(
+        //           source: ImageSource.gallery,
+        //           imageQuality: 100);
+        //       setState(() {
+        //         if (pickedimage != null) {
+        //           img_present = true;
+        //           image = File(pickedimage.path);
+        //         }
+        //       });
+        //     },
+        //     child: Icon(
+        //       Icons.image,
+        //       color: Colors.teal[600],
+        //     ),
+        //     style: OutlinedButton.styleFrom(
+        //       //backgroundColor: Colors.grey[200],
+        //       shape: CircleBorder(),
+        //       padding: EdgeInsets.all(14),
+        //     ),
+        //   ),
+        // ]),
