a8e6330f36827c44f420add9e04a3ab3efa8ccfe
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-08-01 22:57:20 +0530

    prediction in form of Table

--- mlflutter/lib/widgets/prediction.dart
+++ mlflutter/lib/widgets/prediction.dart
@@ -2,100 +2,98 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:mlflutter/widgets/classifier.dart';
 
 class Prediction extends StatefulWidget {
-
   const Prediction({Key key, this.imageClicked}) : super(key: key);
   final File imageClicked;
-
-  
 
   @override
   _PredictionState createState() => _PredictionState();
 }
 
 class _PredictionState extends State<Prediction> {
-
-  File imageClicked;
+  final Classifier classifier = Classifier();
+  Map prediction;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Center(
+    return Column(children: [
+      Center(
           child: ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              onPrimary: Colors.black87,
-              primary: Colors.grey[700],
-            ),
-            onPressed: () {
-              
-            },
-            child: Text(
-              'Prediction',
-                style: TextStyle(
-                color: Colors.grey[100],
-                fontSize: 28,
-                fontWeight: FontWeight.w100,
-                letterSpacing: 4,
-                fontFamily: GoogleFonts.satisfy().fontFamily,
-              ),
-            ),
-          )
+        style: ElevatedButton.styleFrom(
+          onPrimary: Colors.black12,
+          primary: Colors.teal[200],
         ),
-        // Center(
-        //   child: Text(
-        //     'Covid 19: ',
-        //     style: TextStyle(
-        //       color: Colors.grey[700],
-        //       fontSize: 18,
-        //       fontWeight: FontWeight.w300,
-        //       letterSpacing: 1,
-        //       fontStyle: FontStyle.italic,
-        //       fontFamily: GoogleFonts.comicNeue().fontFamily,
-        //     ),
-        //   ) ,
-        // ),
-        // Center(
-        //   child: Text(
-        //     'Lung Opacity: ',
-        //     style: TextStyle(
-        //       color: Colors.grey[700],
-        //       fontSize: 18,
-        //       fontWeight: FontWeight.w300,
-        //       letterSpacing: 1,
-        //       fontStyle: FontStyle.italic,
-        //       fontFamily: GoogleFonts.comicNeue().fontFamily,
-        //     ),
-        //   ) ,
-        // ),
-        // Center(
-        //   child: Text(
-        //     'Viral Pneumonia: ',
-        //     style: TextStyle(
-        //       color: Colors.grey[700],
-        //       fontSize: 18,
-        //       fontWeight: FontWeight.w300,
-        //       letterSpacing: 1,
-        //       fontStyle: FontStyle.italic,
-        //       fontFamily: GoogleFonts.comicNeue().fontFamily,
-        //     ),
-        //   ) ,
-        // ),
-        // Center(
-        //   child: Text(
-        //     'Normal: ',
-        //     style: TextStyle(
-        //       color: Colors.grey[700],
-        //       fontSize: 18,
-        //       fontWeight: FontWeight.w300,
-        //       letterSpacing: 1,
-        //       fontStyle: FontStyle.italic,
-        //       fontFamily: GoogleFonts.comicNeue().fontFamily,
-        //     ),
-        //   ) ,
-        // ),
-      ]
-    );
+        onPressed: () async {
+          final outputs = await classifier.classifyImage(widget.imageClicked);
+
+          setState(() {
+            prediction = outputs;
+          });
+        },
+        child: Text(
+          'Predict',
+          style: TextStyle(
+            color: Colors.grey[900],
+            fontSize: 30,
+            fontWeight: FontWeight.w100,
+            letterSpacing: 3,
+            fontFamily: GoogleFonts.niconne().fontFamily,
+          ),
+        ),
+      )),
+      SizedBox(height: 20),
+      Table(
+        defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+        border: TableBorder.all(
+          color: Colors.teal[800],
+          width:2,
+          
+          
+        ), 
+        columnWidths: {
+        0: FixedColumnWidth(MediaQuery.of(context).size.width * 0.4),
+        1: FixedColumnWidth(MediaQuery.of(context).size.width * 0.4)
+        }, 
+
+        children: [
+          for (var k in prediction.entries)
+            TableRow(
+              
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(6.0),
+                  child: Center(
+                    child: Text(
+                      k.key,
+                      style: TextStyle(
+                        fontFamily: GoogleFonts.courgette().fontFamily,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16
+                      ),
+
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(6.0),
+                  child: Center(
+                    child: Text(
+                      "${k.value*100}".substring(0,5)+" %",
+                      style: TextStyle(
+                        fontFamily: GoogleFonts.courgette().fontFamily,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16
+                      )
+                    ),
+                  ),
+                ),
+                //Text("${double.parse(k.value.toStringAsFixed(2))}"),
+              ]
+            )
+        ]
+      )
+    ]);
   }
 }
