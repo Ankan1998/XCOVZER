695d836dd9ae6d3e99ccbaa2aaeb113324f994f4
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-08-01 12:32:08 +0530

    tflite converter script added

---
+++ ML/tflite_converter.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+saved_model_dir = r'C:\Users\Ankan\Desktop\Github\ML-with-Flutter\ML\best_model'
+converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir)
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+converter.target_spec.supported_types = [tf.float16]
+tflite_model = converter.convert()
+
+with open('converted_model_tflite', 'wb') as f:
+    f.write(tflite_model)
