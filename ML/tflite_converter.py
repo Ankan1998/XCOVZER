7701fa713a4b62ead5b983ee2c737a706dc0dcbe
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-08-02 21:22:47 +0530

    tflite_converter updated

--- ML/tflite_converter.py
+++ ML/tflite_converter.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-saved_model_dir = r'C:\Users\Ankan\Desktop\Github\ML-with-Flutter\ML\best_model'
+saved_model_dir = r'C:\Users\Ankan\Desktop\Github\ML-with-Flutter\ML\best_weightv2'
 converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir)
 converter.optimizations = [tf.lite.Optimize.DEFAULT]
 converter.target_spec.supported_types = [tf.float32]
